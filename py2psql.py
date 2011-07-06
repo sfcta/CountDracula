16238c8b4235bcf34d444fbd3fb9821fa3377871
Varun Kohli <varun.kohli@sfcta.org>
2011-07-06 14:44:11 -0700

    Cleaned and Commented

--- py2psql.py
+++ py2psql.py
@@ -21,7 +21,6 @@ def upload_mainline (commandslist,db,user):	    #uploads counts to mainline tabl
     
     for command in commandslist:
         #send command to server
-        #cur2db.execute("INSERT INTO counts_ml (count,starttime,period,vtype, onstreet,ondir,fromstreet,tostreet,refpos,sourcefile,project) Values (%s, %s, %s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s )",
         cur2db.execute("INSERT INTO counts_ml (count,starttime,period,vtype, onstreet,ondir,fromstreet,tostreet,refpos,sourcefile,project) Values (%s, %s, %s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s )",
                        tuple(command))
         
@@ -39,29 +38,28 @@ def upload_turns (commandslist,db,user):	#uploads counts to turns table
     #cur2db.execute("DELETE from counts_turns;")
     
     for command in commandslist:
-        #send command to server
+        #-----Check if intersection exists in DB-------------- 
         cur2db.execute("SELECT int_id from intersection_ids WHERE ((street1=%s AND street2=%s) OR (street1=%s AND street2=%s));",(command[4],command[8],command[8],command[4]))
         intid = -1 
         intid = cur2db.fetchone()
-        if intid == None:
+        if intid == None:   #i.e. intersection not found !!
             print ('INTERSECTION with streets 1) '+command[4]+' 2) '+command[8]+' NOT IN DB')
-            raise
+            raise   #raise exception
         else:
-        #cur2db.execute("INSERT INTO counts_turns (count,starttime,period,vtype,fromstreet,fromdir,tostreet,todir,intstreet,intid,sourcefile,project) Values (%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s )",
+            #send command to server
             cur2db.execute("INSERT INTO counts_turns (count,starttime,period,vtype,fromstreet,fromdir,tostreet,todir,intstreet,intid,sourcefile,project) Values (%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s )",
                             (command[0],command[1],command[2],command[3],command[4],command[5],command[6],command[7],command[8],intid,command[10],command[11],))
-        
-            
     conn2db.commit()
     cur2db.close()
     conn2db.close()
 
-def street_names (commandslist,db,user):    #uploads street names to  table
+def street_names (commandslist,db,user):    #uploads street names to  DB
     
     conn2db = psycopg2.connect("dbname="+db+" user="+user)
     cur2db = conn2db.cursor()
     
     #________________THIS IS ONLY FOR TESTING !!!
+    #OR Clear DB before uploading new street_names
     cur2db.execute("DELETE from counts_ml;")
     cur2db.execute("DELETE from counts_turns;")
     cur2db.execute("DELETE from intersection_ids;")
@@ -69,10 +67,8 @@ def street_names (commandslist,db,user):    #uploads street names to  table
     
     for command in commandslist:
         #send command to server
-        #cur2db.execute("INSERT INTO counts_turns (count,starttime,period,vtype,fromstreet,fromdir,tostreet,todir,intstreet,intid,sourcefile,project) Values (%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s ,%s )",
         cur2db.execute("INSERT INTO street_names VALUES (%s)",
                        [command])
-        
     conn2db.commit()
     cur2db.close()
     conn2db.close()
@@ -83,18 +79,14 @@ def int_ids (commandslist,db,user):    #uploads intersection ids table
     cur2db = conn2db.cursor()
     
     #________________THIS IS ONLY FOR TESTING !!!
+    #OR EMPTY EXISTING INTERSECTIONS BEFORE ENTERING NEW TABLE
     cur2db.execute("DELETE from intersection_ids;")
     
     
     for command in commandslist:
-        #send command to server
-        #cur2db.execute("INSERT INTO intersection_ids VALUES (%s, %s, %s)",
-        #=======================================================================
-        # ONLY IF PAIR DOESNT EXIST
-        #=======================================================================
+        #ONLY done if street-pair doesn't exist already 
         cur2db.execute("INSERT INTO intersection_ids (street1, street2, int_id) SELECT %s, %s, %s WHERE NOT EXISTS (SELECT street1, street2 FROM intersection_ids WHERE street1 = %s AND street2 = %s);",
                            (command[0],command[1],command[2],command[0],command[1]))
-            
     conn2db.commit()
     cur2db.close()
     conn2db.close()

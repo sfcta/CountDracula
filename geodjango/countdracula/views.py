bc9f5385c49cde1b8b8bd14fd32626121fef8ae3
Lisa Zorn <lisa.zorn@sfcta.org>
2013-04-11 13:22:45 -0700

    Enable vehicle-type based filtering for movement counts also. Implement 4 download types: movement|mainline, all counts|filtered (for the marked locations)

--- geodjango/countdracula/views.py
+++ geodjango/countdracula/views.py
@@ -33,19 +33,40 @@ def mapview(request):
     turn_years = TurnCount.objects.values_list('count_year',flat=True).distinct();
     count_years = sorted(set(ml_years).union(set(turn_years)))
     
-    # get a mapping of loc_id -> [ years ]
+    # get a mapping of loc_id -> { year:vehtype }
     turnlocs_to_years = {}
-    tcqs = TurnCountLocation.objects.values('id','turncount__count_year').distinct()
+    tcqs = TurnCountLocation.objects.values('id','turncount__count_year', 'turncount__vehicle_type').distinct()
     for row in tcqs:
-        if row['id'] not in turnlocs_to_years: turnlocs_to_years[row['id']] = []
-        turnlocs_to_years[row['id']].append(row['turncount__count_year'])
+        # init for id
+        if row['id'] not in turnlocs_to_years: turnlocs_to_years[row['id']] = {}
+        # init for id, year
+        if row['turncount__count_year'] not in turnlocs_to_years[row['id']]:
+            turnlocs_to_years[row['id']][row['turncount__count_year']] = 0
+        # bitwise or
+        if row['turncount__vehicle_type'] >= 6:
+            turnlocs_to_years[row['id']][row['turncount__count_year']] |= (1 << 3)  # truck
+        elif row['turncount__vehicle_type'] >= 0:
+            turnlocs_to_years[row['id']][row['turncount__count_year']] |= (1 << row['turncount__vehicle_type'])
+        else:
+            print row
+    del tcqs
     
     mainlinelocs_to_years = {}
-    mlqs = MainlineCountLocation.objects.values('id','mainlinecount__count_year').distinct()
+    mlqs = MainlineCountLocation.objects.values('id','mainlinecount__count_year','mainlinecount__vehicle_type').distinct()
     for row in mlqs:
-        if row['id'] not in mainlinelocs_to_years: mainlinelocs_to_years[row['id']] = []
-        mainlinelocs_to_years[row['id']].append(row['mainlinecount__count_year'])
-    
+        # init for id
+        if row['id'] not in mainlinelocs_to_years: mainlinelocs_to_years[row['id']] = {}
+        # init for id, year
+        if row['mainlinecount__count_year'] not in mainlinelocs_to_years[row['id']]:
+            mainlinelocs_to_years[row['id']][row['mainlinecount__count_year']] = 0
+        # bitwise or
+        if row['mainlinecount__vehicle_type'] >= 6:
+            mainlinelocs_to_years[row['id']][row['mainlinecount__count_year']] |= (1 << 3)  # truck
+        elif row['mainlinecount__vehicle_type'] >= 0:
+            mainlinelocs_to_years[row['id']][row['mainlinecount__count_year']] |= (1 << row['mainlinecount__vehicle_type'])
+        else:
+            print row
+    del mlqs
     
     print "mapview queries done: " + str(datetime.datetime.now())
     
@@ -109,27 +130,65 @@ def counts_for_location(request):
 def download(request):
     
     response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename="CountDraculaDownload.csv"'
+    response['Content-Disposition'] = 'attachment; filename="CountDracula_%s.csv"' % \
+        ("Mainline" if request.POST[u'download-type'] in [u'mainlinelocs', u'mainlinefilt'] else "Movement")
     writer = csv.writer(response)
 
-    # print request.POST.getlist(u'count-type')
-    # print request.POST.getlist(u'count-year')
-    
-    # transform the count-year to a list
-    count_years = []
-    for arg in request.POST.getlist(u'count-year'):
-        count_years.append(int(arg[4:]))
+    count_years = None
+    vehicle_types = None
+    if request.POST[u'download-type'] in [u'mainlinefilt', u'movementfilt']:
+        # transform the count-year to a list
+        count_years = []
+        for arg in request.POST.getlist(u'count-year'):
+            count_years.append(int(arg[4:]))
+        
+        # transform the vehicle types into a list
+        vehicle_types = []
+        for arg in request.POST.getlist(u'vtype'):
+            vehicle_types.append(int(arg))
+            # truck detail
+            if int(arg)==3:
+                vehicle_types.extend([6,7,8,9,10,11,12,13,14,15])
+        
+    location_ids = []
+    if request.POST[u'download-type'] in [u'mainlinelocs', u'mainlinefilt']:
 
-    if u'mainline' in request.POST.getlist(u'count-type'):
-        # fetch mainline data
-        mlqs = MainlineCount.objects.select_related().filter(count_year__in=count_years).order_by('location')
+        for arg in request.POST[u'mainline_loc_ids'].split(','): location_ids.append(int(arg))
+        # fetch mainline data        
+        mlqs = MainlineCount.objects.select_related().filter(location_id__in=location_ids).order_by('location')
+                    
+    else:
+        for arg in request.POST[u'movement_loc_ids'].split(','): location_ids.append(int(arg))
+        # fetch turn data
+        mlqs = TurnCount.objects.select_related().filter(location_id__in=location_ids).order_by('location')
+        
+
+    # log it
+    writer.writerow(["LocationID in %s" % str(location_ids)])
+
+    # further filter it    
+    if request.POST[u'download-type'] in [u'mainlinefilt', u'movementfilt']:
+        mlqs = mlqs.filter(count_year__in=count_years).filter(vehicle_type__in=vehicle_types)
+        # log it
+        writer.writerow(["Count Year in %s" % str(count_years)])
+        writer.writerow(["Vehicle Type in %s" % str(vehicle_types)])
     
-        # header row
+    
+    # header row
+    if request.POST[u'download-type'] in [u'mainlinelocs', u'mainlinefilt']:
+        
         writer.writerow(["LocationID", "Location OnStreet", "Location OnDir", "Location FromStreet", "Location FromNode", 
                          "Location ToStreet", "Location ToNode",
                          "Count", "Count Date", "Count Year", "Start Time", "Period Minutes", "Vehicle Type",
                          "Source File", "Project", "Reference Position", "Upload User"])
-        for mcount in mlqs:
+    else:
+        writer.writerow(["LocationID", "Location FromStreet", "Location FromDir", "Location ToStreet", "Location ToDir", 
+                         "Location IntStreet", "Location IntNode",
+                         "Count", "Count Date", "Count Year", "Start Time", "Period Minutes", "Vehicle Type",
+                         "Source File", "Project", "Upload User"])
+                
+    for mcount in mlqs:
+        if request.POST[u'download-type'] in [u'mainlinelocs', u'mainlinefilt']:
             writer.writerow([mcount.location.id,
                              mcount.location.on_street,
                              mcount.location.on_dir,
@@ -147,4 +206,23 @@ def download(request):
                              mcount.project,
                              mcount.reference_position,
                              mcount.upload_user])
+    
+        else:
+            writer.writerow([mcount.location.id,
+                             mcount.location.from_street,
+                             mcount.location.from_dir,
+                             mcount.location.to_street,
+                             mcount.location.to_dir,
+                             mcount.location.intersection_street,
+                             mcount.location.intersection,
+                             mcount.count,
+                             mcount.count_date,
+                             mcount.count_year,
+                             mcount.start_time,
+                             mcount.period_minutes,
+                             mcount.vehicle_type,
+                             mcount.sourcefile,
+                             mcount.project,
+                             mcount.upload_user])
+
     return response    
